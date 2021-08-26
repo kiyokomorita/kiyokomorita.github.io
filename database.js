c47459fe4ccb3cc21403ce6dddedfd19f4992a7a
Kiyoko <kiyoko113@yahoo.co.jp>
2021-08-25 23:36:54 -0700

    draft

---
+++ database.js
@@ -0,0 +1,228 @@
+let db = {
+       
+Cleansers:{
+    title:"Squalane Cleanser",
+    Brand:"The Ordinary",
+    Type:"Skincare",
+    Price:7.90,
+    Date:2021/07/21,
+    img:"https://www.sephora.com/productimages/sku/s2238988-main-zoom.jpg?imwidth=630"
+},
+Cleansers:{
+    title:"All About Clean™ Liquid Facial Soap",
+    Brand:"CLINIQUE",
+    Type:"Skincare",
+    Price:26.00,
+    Date:2020/01/21,
+    img:"https://www.sephora.com/productimages/sku/s899070-main-zoom.jpg?imwidth=630"
+},
+Cleansers:{
+    title:"Facial Treatment Cleanser",
+    Brand:"SK-II",
+    Type:"Skincare",
+    Price:99.00,
+    Date:2019/03/21,
+    img:"https://www.sephora.com/productimages/sku/s1448596-main-zoom.jpg?imwidth=3000"
+},
+Moisturizers:{
+    title:"Vitamin C Suspension 23% + HA Spheres 2%",
+    Brand:"The Ordinary",
+    Type:"Skincare",
+    Price:5.80,
+    Date:2019/03/21,
+    img:"https://www.sephora.com/productimages/sku/s2031466-main-zoom.jpg?imwidth=630"
+
+},
+Moisturizers:{
+    title:"Ultra Facial Moisturizing Cream with Squalane",
+    Brand:"Kiehl's",
+    Type:"Skincare",
+    Price:40.00,
+    Date:2021/07/21,
+    img:"https://www.sephora.com/productimages/sku/s2188902-main-zoom.jpg?imwidth=630"
+
+},
+Moisturizers:{
+    title:"Crème de la Mer Moisturizer",
+    Brand:"Lar Mer",
+    Type:"Skincare",
+    Price:255.00,
+    Date:2021/03/21,
+    img:"https://www.sephora.com/productimages/sku/s1932201-main-zoom.jpg?imwidth=630"
+
+},
+Treatments:{
+    title:"Natural Moisturizing Factors + HA",
+    Brand:"The Ordinary",
+    Type:"Skincare",
+    Price:5.80,
+    Date:2018/07/21,
+    img:"https://www.sephora.com/productimages/sku/s2031425-main-zoom.jpg?imwidth=3000"
+},
+Treatments:{
+    title:"The Treatment Lotion",
+    Brand:"La Mer",
+    Type:"Skincare",
+    Price:215.00,
+    Date:2020/07/21,
+    img:"https://www.sephora.com/productimages/sku/s1932391-main-zoom.jpg?imwidth=630"
+},
+Treatments:{
+    title:"Facial Treatment Essence (Pitera Essence)",
+    Brand:"SK-II",
+    Type:"Skincare",
+    Price:241.00,
+    Date:2021/07/21,
+    img:"https://www.sephora.com/productimages/sku/s1850346-main-zoom.jpg?imwidth=630"
+},
+EyeCare:{
+    title:"Advanced Night Repair Eye Supercharged Complex",
+    Brand:"Estée Lauder",
+    Type:"Skincare",
+    Price:87.00,
+    Date:2021/02/21,
+    img:"https://www.sephora.com/productimages/sku/s2111854-main-zoom.jpg?imwidth=1224"
+},
+EyeCare:{
+    title:"All About Eyes™ Rich Eye Cream",
+    Brand:"CLINIQUE",
+    Type:"Skincare",
+    Price:45.00,
+    Date:2020/10/21,
+    img:"https://www.sephora.com/productimages/sku/s971473-main-zoom.jpg?imwidth=1224"
+},
+EyeCare:{
+    title:"Absolute Revitilizing Eye Cream",
+    Brand:"Lancôme",
+    Type:"Skincare",
+    Price:164.00,
+    Date:2020/02/21,
+    img:"https://www.sephora.com/productimages/sku/s2258036-main-zoom.jpg?imwidth=1224"
+},
+
+Foundation:{
+    title:"Natural Radiant Longwear Foundation",
+    Brand:"NARS",
+    Type:"Makeup",
+    Price:63.00,
+    Date:2021/07/21,
+    img:"https://www.sephora.com/productimages/sku/s2031011-main-zoom.jpg?imwidth=1224"
+},
+Foundation:{
+    title:"Skin Long-Wear Weightless Foundation SPF 15",
+    Brand:"Bobbi Brown",
+    Type:"Makeup",
+    Price:62.00,
+    Date:2021/01/21,
+    img:"https://www.sephora.com/productimages/sku/s2032845-main-zoom.jpg?imwidth=3000"
+},
+Foundation:{
+    title:"Even Better™ Makeup Broad Spectrum SPF 15 Foundation",
+    Brand:"CLINIQUE",
+    Type:"Makeup",
+    Price:40.00,
+    Date:2021/03/21,
+    img:"https://www.sephora.com/productimages/sku/s2083889-main-zoom.jpg?imwidth=3000"
+},
+
+Eyeshadow:{
+    title:"Let's Go Crazy Eyeshadow Palette - Prince Collection",
+    Brand:"Urban Decay",
+    Type:"Makeup",
+    Price:72.00,
+    Date:2021/08/01,
+    img:"https://www.sephora.com/productimages/sku/s2455954-main-zoom.jpg?imwidth=1224"
+},
+Eyeshadow:{
+    title:"Eye Shadow",
+    Brand:"MAC Cosmetics",
+    Type:"Makeup",
+    Price:22.00,
+    Date:2020/08/21,
+    img:"https://www.sephora.com/productimages/sku/s2090439-main-zoom.jpg?imwidth=3000"
+},
+Eyeshadow:{
+    title:"MINERALIST Eyeshadow Palette",
+    Brand:"bareMinerals",
+    Type:"Makeup",
+    Price:42.00,
+    Date:2018/06/21,
+    img:"https://www.sephora.com/productimages/sku/s2477206-main-zoom.jpg?imwidth=3000"
+},
+
+Mascara:{
+    title:"Roller Lash Curling & Lifting Mascara ",
+    Brand:"Benefit Cosmetics",
+    Type:"Makeup",
+    Price:34.00,
+    Date:2019/10/21,
+    img:"https://www.sephora.com/productimages/sku/s1665082-main-zoom.jpg?imwidth=3000"
+},
+Mascara:{
+    title:"Limitless Lash Lengthening Mascara",
+    Brand:"ILIA",
+    Type:"Makeup",
+    Price:37.00,
+    Date:2021/07/21,
+    img:"https://www.sephora.com/productimages/sku/s2335198-main-zoom.jpg?imwidth=3000"
+},
+Mascara:{
+    title:"High Impact Mascara",
+    Brand:"CLINIQUE",
+    Type:"Makeup",
+    Price:28.00,
+    Date:2019/05/21,
+    img:"https://www.sephora.com/productimages/sku/s711267-main-zoom.jpg?imwidth=3000"
+},
+
+Cheek:{
+    title:"Powder Blush",
+    Brand:"MAC Cosmetics",
+    Type:"Makeup",
+    Price:32.00,
+    Date:2020/05/21,
+    img:"https://www.sephora.com/productimages/sku/s2094035-main-zoom.jpg?imwidth=3000"
+},
+Cheek:{
+    title:"BACKSTAGE Glow Face Palette",
+    Brand:"Dior",
+    Type:"Makeup",
+    Price:55.00,
+    Date:2021/06/30,
+    img:"https://www.sephora.com/productimages/sku/s2087849-main-zoom.jpg?imwidth=3000"
+},
+Cheek:{
+    title:"Liquid Blush",
+    Brand:"NARS",
+    Type:"Makeup",
+    Price:39.00,
+    Date:2019/06/21,
+    img:"https://www.sephora.com/productimages/sku/s1943331-main-zoom.jpg?imwidth=3000"
+},
+
+Lipstick:{
+    title:"Matte Lipstick",
+    Brand:"MAC Cosmetics",
+    Type:"Makeup",
+    Price:25.00,
+    Date:2018/08/21,
+    img:"https://www.sephora.com/productimages/sku/s2200525-main-zoom.jpg?imwidth=1224"
+},
+Lipstick:{
+    title:"Rouge Dior Forever Liquid Transfer-Proof Lipstick",
+    Brand:"Dior",
+    Type:"Makeup",
+    Price:48.00,
+    Date:2021/04/21,
+    img:"https://www.sephora.com/productimages/sku/s2470391-main-zoom.jpg?imwidth=1224"
+},
+Lipstick:{
+    title:"Rouge Volupté Shine Lipstick Balm",
+    Brand:"Yves Saint Laurent",
+    Type:"Makeup",
+    Price:50.00,
+    Date:2020/09/21,
+    img:"https://www.sephora.com/productimages/sku/s2427235-main-zoom.jpg?imwidth=1224"
+},
+
+}
